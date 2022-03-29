d40e06ef787a5b37d47d7885b783eb88e924dc38
AFURO-1130 <softtennis6161130@icloud.com>
2022-03-30 00:59:55 +0900

    少しリファクタリング

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <video id="my-video" width="400px" autoplay muted playsinline></video>
     <p>{{ peerId }}</p>
-    <input id="their-id" />
+    <input id="their-id" v-model="theirId" />
     <button @click="makeCall()">発信</button>
     <video id="their-video" width="400px" autoplay muted playsinline></video>
   </div>
@@ -17,12 +17,15 @@ export default {
       APIKey: 'b160e98d-c322-4a0a-83df-b5c4e86c8891',
       peer: '',
       peerId: '',
+      theirID:''
     }
   },
   methods: {
     makeCall() {
-      const theirID = document.getElementById('their-id').value
-      const mediaConnection = this.peer.call(theirID, this.localStream)
+      // const theirID = document.getElementById('their-id').value
+      // console.log('theirID',theirID)
+      // console.log('hoeg')
+      const mediaConnection = this.peer.call(this.theirId, this.localStream)
       this.setEventListener(mediaConnection)
     },
     setEventListener(mediaConnection) {
